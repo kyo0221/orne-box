bea285ae6257d279399fdebd9e7f4e8cd52299a9
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-09-13 20:19:54 +0900

    🐛fix: frame_id修正

--- orne_box_navigation_executor/launch/pcd_to_pointcloud.launch.py
+++ orne_box_navigation_executor/launch/pcd_to_pointcloud.launch.py
@@ -3,23 +3,17 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    nav_dir = get_package_share_directory('orne_box_navigation_executor')
-    config_dir = os.path.join(nav_dir, 'config')
-    default=os.path.join(
-            config_dir,
-            'maps',
-            '3F-map.pcd')
-    
+def generate_launch_description():    
     return LaunchDescription([
         Node(
             package='pcl_ros',
             executable='pcd_to_pointcloud',
             name='pcd_to_pointcloud',
             output='screen',
-            parameters=[
-                {'file_name': '/home/kyo/orne_ws/src/orne-box/orne_box_navigation_executor/config/maps/3F_map.pcd'}  # PCDファイルのパスを指定
-            ],
+            parameters=[{
+                'file_name': '/home/kyo/orne_ws/src/orne-box/orne_box_navigation_executor/config/maps/3F_map.pcd',
+                'tf_frame': 'map'
+            }],
             remappings=[
                 ('/cloud_pcd', '/map')
             ]
